778119a4f687d92c42465ac51cb7e7003111d5cd
Stefania Mindrusca <stefi.mindrusca@gmail.com>
2024-12-01 21:55:10 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace MindruscaStefaniaLab7
+﻿using System;
+using MindruscaStefaniaLab7.Data;
+using System.IO;
+
+namespace MindruscaStefaniaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+    ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
---
+++ listView.cs
@@ -0,0 +1,9 @@
+﻿using MindruscaStefaniaLab7.Models;
+
+namespace MindruscaStefaniaLab7
+{
+    internal class listView
+    {
+        public static List<ShopList> ItemsSource { get; internal set; }
+    }
+}
